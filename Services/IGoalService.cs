6323cfcfcd707327e3c4db587fed1deb3f804121
Samuel Molla <sambenmayu@gmail.com>
2025-11-28 19:42:03 -0800

    Add Goal Services

---
+++ Services/IGoalService.cs
@@ -0,0 +1,10 @@
+﻿using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Services;
+
+public interface IGoalService
+{
+    Task<IReadOnlyList<Goal>> GetGoalsAsync();
+    Task AddOrUpdateGoalAsync(Goal goal);
+    Task DeleteGoalAsync(int id);
+}
