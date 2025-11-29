df889cf4e0d82ddc38df394b53fa5e462330d949
Samuel Molla <sambenmayu@gmail.com>
2025-11-28 19:40:54 -0800

    Add Goal Model

---
+++ Models/Goal.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace ExpenseTracker.Models;
+
+public class Goal
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;       
+    public decimal TargetAmount { get; set; }               
+    public decimal CurrentAmount { get; set; }              
+    public DateTime? Deadline { get; set; }                
+    public bool IsCompleted { get; set; }
+}
